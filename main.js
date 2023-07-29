0231af4a5edeaf7fd28eaec7cdd0a4db932153c0
Sabrina B <sabrow@hotmail.fr>
2023-07-29 11:59:52 +0200

    perf: uninstall discord.js

---
+++ main.js
@@ -0,0 +1,3 @@
+import { REST } from '@discordjs/rest';
+import { WebSocketManager } from '@discordjs/ws';
+import { GatewayDispatchEvents, GatewayIntentBits, InteractionType, MessageFlags, Client } from '@discordjs/core';
